4f18009bda13b6e662e0deac255bd2a788be92ca
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-09-27 10:51:13 -0500

    got the schema files setup and ready for code

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+//* Importing to export my typeDefs and resolvers
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+//* Exporting my typeDefs and resolvers
+module.exports = { typeDefs, resolvers };
